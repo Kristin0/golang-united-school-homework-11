2b0b771dae74ddec7b6546ab5e095f5438d994e4
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-07-18 00:10:11 +0400

    Initial commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -2,6 +2,7 @@ package batch
 
 import (
 	"time"
+	"sync"
 )
 
 type user struct {
@@ -14,5 +15,20 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
+	const paralel = 5
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, paralel)
+	for i := 0; int64(i) < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func (){
+			for j := 0; int64(j) < n; j++ {
+				getOne(int64(j))
+			}
+		  <- sem
+		  wg.Done()
+		}()
+	}
+	wg.Wait()
 	return nil
 }
