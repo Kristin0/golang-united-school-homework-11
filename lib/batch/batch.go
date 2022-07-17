6f54ed1284e08d90240372d3f38c202cbc1f06a1
Kristin0 <44585557+Kristin0@users.noreply.github.com>
2022-07-18 02:40:27 +0400

    Update batch.go

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -2,7 +2,6 @@ package batch
 
 import (
 	"time"
-	"sync"
 )
 
 type user struct {
@@ -15,20 +14,5 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	const paralel = 5
-	var wg sync.WaitGroup
-	sem := make(chan struct{}, paralel)
-	for i := 0; int64(i) < n; i++ {
-		wg.Add(1)
-		sem <- struct{}{}
-		go func (){
-			for j := 0; int64(j) < n; j++ {
-				getOne(int64(j))
-			}
-		  <- sem
-		  wg.Done()
-		}()
-	}
-	wg.Wait()
 	return nil
 }
